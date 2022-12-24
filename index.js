c8d4ea709434a79fe3a659a01b455485687ac1a2
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-12-24 18:36:56 -0300

    usando o res.send pra colocar o corpo do psot na tela

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ app.listen("3000", console.log("rodando"));
 app.use(express.json())
 app.route('/').get((req, res) => res.send("hello"))
 app.route('/sobre').get((req, res) => res.send(";3"))
-app.route('/').post((req, res) => console.log(req.body))
+app.route('/').post((req, res) => res.send(req.body))
