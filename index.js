638af90db764ad96e4e66ff4043b50150b5e1443
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-12-24 18:35:35 -0300

    require do express. criação do listen , routes e agora testando o .post)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.listen("3000", console.log("rodando"));
-
+app.use(express.json())
 app.route('/').get((req, res) => res.send("hello"))
 app.route('/sobre').get((req, res) => res.send(";3"))
+app.route('/').post((req, res) => console.log(req.body))
