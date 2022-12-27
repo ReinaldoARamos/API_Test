6211179178a6c9f09882409eed977df93ecd5e30
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-12-26 21:44:18 -0300

    testanto req.paramns

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const app = express();
 
 app.listen("3000", console.log("rodando"));
 app.use(express.json())
+
 app.route('/').get((req, res) => res.send("hello"))
+
 app.route('/sobre').get((req, res) => res.send(";3"))
-app.route('/').post((req, res) => res.send(req.body))
+
+app.route('/').post((req, res) => {
+    res.send(req.body.nome)
+})
+
 app.route('/:identificador').delete((res,req) =>{
     res.send(req.params.identificador)
 })
